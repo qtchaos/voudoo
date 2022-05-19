e7f541fc09a2b05e5e8453a1fd22a847eba8470e
qtchaos <72168435+qtchaos@users.noreply.github.com>
2022-05-20 00:37:01 +0300

    🧃 Fix robots.txt location and accessibility option [skip ci]

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, send_from_directory, request
 from flask_htmlmin import HTMLMIN
 from flask_minify import Minify
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 app.config['MINIFY_HTML'] = True
 htmlmin = HTMLMIN(app)
 Minify(app=app, html=True, js=True, cssless=True)
@@ -31,6 +31,11 @@ def music():
 @app.route('/stolenflame')
 def youtube():
     return redirect('https://www.youtube.com/watch?v=sIWhkI9O8EI', 308)
+
+
+@app.route('/robots.txt')
+def robots():
+    return send_from_directory(app.static_folder, request.path[1:])
 
 
 @app.errorhandler(404)
